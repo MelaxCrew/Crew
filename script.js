87dfacfad86bf88c6e6389d5e13d92131a72b265
MelaxCrew <150865246+MelaxCrew@users.noreply.github.com>
2023-11-15 18:59:42 +0100

    script.js

--- script.js
+++ script.js
@@ -2,35 +2,114 @@ const catalogElement = document.getElementById('catalog');
 const previewContainer = document.getElementById('preview-container');
 const previewContent = document.getElementById('preview-content');
 const previewImage = document.getElementById('preview-image');
+const previewDescription = document.getElementById('preview-description');
 
-// Ejemplo de datos con URL de productos
+// Ejemplo de datos con URL de productos y descripciones
 const products = [
-  { image: 'ruta-a-tu-imagen-1.jpg' },
-  { image: 'ruta-a-tu-imagen-2.jpg' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_hyper_royal.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_4_white_cement.png', description: 'Descripción del Producto 1' },
+  { image: 'images/yeesy_350_negra.png', description: 'Descripción del Producto 2' },
+  { image: 'images/yeesy_boost_350.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan_1_buble.png', description: 'Descripción del Producto 2' },
   // Agrega más productos según sea necesario
 ];
 
+// Función para acortar el enlace con Bitly
+function shortenLink(longUrl, callback) {
+  const accessToken = '6b65158f020c81b67e6f1aba523f29f3f5674ced'; // Reemplaza con tu token de acceso de Bitly
+  const apiUrl = `https://api-ssl.bitly.com/v4/shorten`;
+
+  $.ajax({
+    url: apiUrl,
+    type: 'POST',
+    dataType: 'json',
+    contentType: 'application/json',
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+    },
+    data: JSON.stringify({
+      long_url: longUrl,
+    }),
+    success: function (response) {
+      const shortUrl = response.id;
+      callback(shortUrl);
+    },
+    error: function (error) {
+      console.error('Error al acortar el enlace con Bitly:', error);
+      callback(longUrl); // En caso de error, usa el enlace largo
+    },
+  });
+}
+
 // Crear elementos de producto y agregar al catálogo
-products.forEach((product, index) => {
+products.forEach(product => {
   const productElement = document.createElement('div');
   productElement.classList.add('product');
 
   const imgElement = document.createElement('img');
   imgElement.src = product.image;
-  imgElement.alt = `Producto ${index + 1}`;
+  imgElement.alt = product.description;
+
+  const nameElement = document.createElement('p');
+  nameElement.textContent = product.description;
 
   productElement.appendChild(imgElement);
-  catalogElement.appendChild(productElement);
+  productElement.appendChild(nameElement);
 
-  // Agrega un manejador de clic para las imágenes del catálogo
-  imgElement.addEventListener('click', () => openPreview(product.image));
+  catalogElement.appendChild(productElement);
+});
+
+// Agrega un manejador de clic para las imágenes del catálogo
+catalogElement.addEventListener('click', (event) => {
+  if (event.target.tagName === 'IMG') {
+    const imageSrc = event.target.src;
+    const description = event.target.alt;
+    openPreview(imageSrc, description);
+  }
 });
 
 // Función para abrir la vista previa
-function openPreview(imageSrc) {
+function openPreview(imageSrc, description) {
   previewImage.src = imageSrc;
-  previewContainer.style.display = 'flex';
+  previewDescription.textContent = description;
+
+  // Obtén el enlace directo a la imagen
+  const directLink = window.location.origin + '/' + imageSrc; // Ajusta según la estructura de tu proyecto
+
+  // Acorta el enlace directo usando Bitly
+  shortenLink(directLink, function (shortenedLink) {
+    // Modifica el enlace de WhatsApp para incluir el enlace acortado a la imagen
+    const whatsappLink = `https://wa.me/34627629079?text=¡Hola! Estoy interesado en comprar este producto: ${description} - ${shortenedLink}`;
+    document.getElementById('whatsapp-btn').href = whatsappLink;
+
+    previewContainer.style.display = 'flex';
+  });
 }
+
+// ...
+
+// Cerrar la vista previa al hacer clic en ella
+previewContainer.addEventListener('click', closePreview);
 
 // Función para cerrar la vista previa
 function closePreview() {
