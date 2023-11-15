5cc3a3efd859cd0488bff73fb5c294ae94fb9c52
MelaxCrew <150865246+MelaxCrew@users.noreply.github.com>
2023-11-15 01:07:40 +0100

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const catalogElement = document.getElementById('catalog');
 const products = [
   { name: 'Producto 1', image: 'URL_imagen_1.jpg' },
   { name: 'Producto 2', image: 'URL_imagen_2.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_3.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_4.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_5.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_6.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_7.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_8.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_9.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_10.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_11.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_12.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_13.jpg' },
+  { name: 'Producto 2', image: 'URL_imagen_14.jpg' },
   // Agrega más productos según sea necesario
 ];
 
