bbd1b564ebb457b50f377b0b68e99567e56ba4b8
MelaxCrew <150865246+MelaxCrew@users.noreply.github.com>
2023-11-15 12:45:59 +0100

    script.js

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ const previewDescription = document.getElementById('preview-description');
 
 // Ejemplo de datos con URL de productos y descripciones
 const products = [
-  { image: 'images/tu-imagen-1.jpg', description: 'Descripción del Producto 1' },
-  { image: 'images/tu-imagen-2.jpg', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 hyper royal.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 4 white cement.png', description: 'Descripción del Producto 1' },
+  { image: 'images/yeesy 350 negra.png', description: 'Descripción del Producto 2' },
+  { image: 'images/yeesy boost 350.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 1' },
+  { image: 'images/jordan 1 buble.png', description: 'Descripción del Producto 2' },
   // Agrega más productos según sea necesario
 ];
 
