ca452cc3fce121bfe77a29f0979bdc928ff3ec47
MelaxCrew <150865246+MelaxCrew@users.noreply.github.com>
2023-11-15 11:01:34 +0100

    script.js

--- script.js
+++ script.js
@@ -2,23 +2,10 @@ const catalogElement = document.getElementById('catalog');
 const previewContainer = document.getElementById('preview-container');
 const previewContent = document.getElementById('preview-content');
 
-// Ejemplo de datos con URL de gatitos
 const products = [
-  { name: 'Gatito 1', image: 'https://placekitten.com/200/200' },
-  { name: 'Gatito 2', image: 'https://placekitten.com/201/200' },
-  { name: 'Gatito 3', image: 'https://placekitten.com/202/200' },
-  { name: 'Gatito 4', image: 'https://placekitten.com/203/200' },
-  { name: 'Gatito 5', image: 'https://placekitten.com/204/200' },
-  { name: 'Gatito 6', image: 'https://placekitten.com/205/200' },
-  { name: 'Gatito 7', image: 'https://placekitten.com/206/200' },
-  { name: 'Gatito 8', image: 'https://placekitten.com/207/200' },
-  { name: 'Gatito 9', image: 'https://placekitten.com/208/200' },
-  { name: 'Gatito 10', image: 'https://placekitten.com/209/200' },
-  { name: 'Gatito 11', image: 'https://placekitten.com/210/200' },
-  { name: 'Gatito 12', image: 'https://placekitten.com/211/200' },
+  // ... (tu array de productos permanece igual)
 ];
 
-// Crear elementos de producto y agregar al catálogo
 products.forEach(product => {
   const productElement = document.createElement('div');
   productElement.classList.add('product');
@@ -36,7 +23,6 @@ products.forEach(product => {
   catalogElement.appendChild(productElement);
 });
 
-// Agrega un manejador de clic para las imágenes del catálogo
 catalogElement.addEventListener('click', (event) => {
   if (event.target.tagName === 'IMG') {
     const imageSrc = event.target.src;
@@ -44,25 +30,19 @@ catalogElement.addEventListener('click', (event) => {
   }
 });
 
-// Función para abrir la vista previa
 function openPreview(imageSrc) {
-  // Limpia el contenido existente
   previewContent.innerHTML = '';
 
-  // Crea la imagen y agrega al contenedor de vista previa
   const previewImage = document.createElement('img');
   previewImage.src = imageSrc;
   previewImage.classList.add('preview-image');
   previewContent.appendChild(previewImage);
 
-  // Muestra el contenedor de vista previa
   previewContainer.style.display = 'flex';
 }
 
-// Cerrar la vista previa al hacer clic en ella
 previewContainer.addEventListener('click', closePreview);
 
-// Función para cerrar la vista previa
 function closePreview() {
   previewContainer.style.display = 'none';
 }
